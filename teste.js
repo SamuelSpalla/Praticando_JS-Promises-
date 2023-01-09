b6dd7051b6ca3d849b8cff16e7a08aecdd849ba9
Samuel Spalla da Silva <118549226+SamuelSpalla@users.noreply.github.com>
2023-01-09 13:31:57 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,38 @@
+var nome = prompt('Qual seu nome')
+alert('É um prazer te conhecer, ' + nome)
+
+const myPromise = new Promise((resolve, reject) => {
+    const nomep = 'Samuel'
+
+    if (nomep === nome){
+        resolve('Usuário Samuel encontrado!')
+    } else{
+        reject(`O usuário ${nome} não foi encontrado`)
+    }
+})
+
+myPromise.then((dados) => {
+    console.log(dados)
+})
+.catch((e) => {
+    console.log('Aconteceu um erro: ' + e)
+})
+
+const userName = 'SamuelSpalla'
+
+fetch(`https://api.github.com/users/${userName}` , {
+    method: 'GET',
+    headers: {
+        Accept: 'application/vnd.github.v3+json',
+    },
+})  .then((response) => {
+        return response.json()
+    })
+    .then((dados) =>{
+        console.log(`O nome de usuário é: ${dados.name}`)
+        console.log(`A bio do usuário é: ${dados.bio}`)
+    })
+    .catch((e) => {
+        console.log(`Houve um erro: ${e}`)
+    })
+    
